be961acb7fa1a25506519eda4ae3b0b3e55607a5
MonaGalal <50083192+MonaHasanin@users.noreply.github.com>
2023-08-06 22:16:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-//Dom Elements
-const circles = document.querySelectorAll(".circle"),
- progressBar = document.querySelector(".indicator"),
- buttons = document.querySelectorAll("button");
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
- let currentStep = 1;
- //function that updates the current step and updates the DOM
- const updateSteps = (e) => {
-    //update current step based on the button clicked
-    currentStep = e.target.id  === "next" ? ++currentStep : --currentStep;
- 
-    //loop through all circles and add/remove "active" class based on their index and current step
-    circles.forEach((circle, index) =>{
-    circle.classList[`${index < currentStep ? "add" : "remove"}`]("active");        
-    });
-    //update progress bar width based on current step
-    progressBar.style.width = `&{((currentStep - 1) / (circles.lenght - 1)) * 100}&`;
-//check if current step is last step or first step and disable corresponding buttons
-    if(currentStep === circles.length) {
-        buttons[1].disabled = true;
-    } else if (currentStep === 1) {
-        buttons[0].disabled = true; 
-    } else {
-        buttons.forEach((button) => (button.disabled = false));
-    } 
-};
-// add click event listeners to all buttons
-buttons.forEach((button) => {
-    button.addEventListener("click", updateSteps);
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) currentActive = circles.length;
+  update();
 });
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) currentActive = 1;
+  update();
+});
+
+const update = () => {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) circle.classList.add("active");
+    else circle.classList.remove("active");
+  });
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+  if (currentActive === 1) prev.disabled = true;
+  else if (currentActive === circles.length) next.disabled = true;
+  else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+};
