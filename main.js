d70b70efcabb271f08563cbe6a175ded4fd14951
MonaGalal <50083192+MonaHasanin@users.noreply.github.com>
2023-08-08 09:22:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -32,3 +32,16 @@ const update = () => {
     next.disabled = false;
   }
 };
+
+let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let colorParts = [];
+for (let i = 0; i < 6; i++) {
+   colorParts.push(hexArray[Math.floor(Math.random() * hexArray.length)])
+}
+//console.log(`#${colorParts.join("")}`);
+let finalColor = `#${colorParts.join("")}`;
+document.body.append(finalColor);
+document.body.style.backgroundColor = finalColor;
+ 
+
+
